baf58fa82f194a79c0b33307f45da13c13fcb4cd
Akila1901 <52735081+Akila1901@users.noreply.github.com>
2021-04-13 16:31:05 +0530

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,5 @@
+get_data<- read.table("C:/Users/Dell/Desktop/coursera/household_power_consumption.txt",skip=1,sep=";")
+names(get_data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+get_datetime <- subset(get_data,get_data$Date=="1/2/2007" | get_data$Date =="2/2/2007")
+hist(as.numeric(as.character(get_datetime$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
